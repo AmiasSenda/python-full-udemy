331c8d40cc559fc7f927168f4aff367edc2b99c5
Amias Senda <amias.senda@faci-x.ao>
2026-07-07 17:29:09 +0100

    example about variaveis hahah

---
+++ logica-de-programacao/aula-08-exec-variaveis.py
@@ -0,0 +1,11 @@
+name = 'Amias'
+Sobrenome = 'Senda'
+age = 26
+AnoNasc = 2000
+altura = 1.75
+maiorIdade = (AnoNasc - age) >= 18
+
+print('Nome Completo: ',name,Sobrenome)
+print('Idade: ',age)
+print('É maior de idade ?', maiorIdade)
+print('Altura em metros: ',altura,'m')
