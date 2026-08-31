70ec4dd58c5957403db62300019d2f6c990ca948
Amias Senda <amiasbento22@gmail.com>
2026-08-31 13:32:11 +0100

    ...

--- projects-simples/pedidos-fastFood/bebidas.py
+++ projects-simples/pedidos-fastFood/bebidas.py
@@ -1,19 +1,35 @@
+import os
+
+quantBebida=0
+precoBebida=0
+totalBebida=0
+
+
 print("EIS As opções de Bebidos da casa")
+print("________________________________")
+print("1- Bebidas Alcoolicas")
+print('2- Sumos naturais')
+print('3- Refrigerantes')
+print("4- Bebidas energéticas")
+print("5- Água")
+print()
 op  =input("digite  o seu  tipo de bebida: ")
 
+
 if (op == '1'):
-    print("1- Bebidas Alcoolicas")
-    print('2- Sumos naturais')
-    print('3- Refrigerantes')
-    print("4- Bebidas energéticas")
-    print("5- Água")
-    op01 = input("digite a Bebida: ")
-if(op == '2'):
+    os.system('cls')
     print("BEBIDAS ALCOOLICAS")
     print("1- Fino | 500,00 kzs")
     print("2- Vinho Tinto | 800,00 kzs")
     print("3- Vinho Branco | 1.050,00 kzs")
+    print()
+    op01 = input("digite a Bebida: ")
+    quantBebida =int(input("Digite a quantidade: "))
+    
+#if(op == '2'):
 
+
+"""
 
 print("SUMOS NATURAIS")
 print("1- Ananás Simples | 500,00 kzs")
@@ -28,4 +44,4 @@ print("3- Sumol de Ananás | 700,00 kzs")
 print("BEBIDAS ENERGÉTICAS")
 print("1- RedBull | 1.500,00 kzs")
 
-
+"""
