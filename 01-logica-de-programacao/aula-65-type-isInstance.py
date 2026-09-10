9c5c0c295046e6f71f5252b28a51b9de64a362b3
Amias Senda <amiasbento22@gmail.com>
2026-09-10 11:57:35 +0100

    ...

---
+++ 01-logica-de-programacao/aula-65-type-isInstance.py
@@ -0,0 +1,31 @@
+developer = 'Amias'
+
+print(type(developer))
+
+account_balance = '12'
+
+print(isinstance(account_balance,int)) #Verifica de a variavel é do tipo int.
+
+my_str_1 = """Muitas Linhas
+Em uma só string.
+.
+Vamos lá ver no que isso vai dar
+"""
+
+print(my_str_1)
+
+msg = "My God is Good!"
+msg_2=  '"Hi!" is greeting'
+
+print(msg)
+print(msg_2)
+
+print("H" in msg_2) #Procura verificar se existe 'H' no conjunto de caracteres da variavel msg_2
+
+print(len(msg_2))
+
+conc = msg
+print(conc)
+conc+=msg_2
+print(conc)
+
